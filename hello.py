5f48609982f93ec7af541939a49bff204c1402b3
liangyuantju <liangyuantju@qq.com>
2019-08-11 11:47:42 +0800

    try use virtual env

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
-msg = "liangyuantju"
-print(msg)
+import matplotlib.pyplot as plt
+import numpy as np
 
-msg1 = "yang"
-print(msg1)
+x = np.linspace(0, 20,100)   # Create a list of evenly-spaced numbers over the range
+plt.plot(x, np.sin(x))       # Plot the sine of each x point
+plt.show()                   # Display the plot
